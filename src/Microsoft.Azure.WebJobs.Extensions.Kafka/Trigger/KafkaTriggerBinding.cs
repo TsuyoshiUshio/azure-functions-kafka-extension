e3b4f8439d917a748800964b122221b88e56ac7b
Kenneth Ham <kenneth.ham@microsoft.com>
2019-04-01 15:46:53 +0800

    [v1.0.1] Implemented Trigger Binding base structure
    - todo: integrate with existing code base
    
    Signed-off-by: Kenneth Ham <kenneth.ham@microsoft.com>

---
+++ src/Microsoft.Azure.WebJobs.Extensions.Kafka/Trigger/KafkaTriggerBinding.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs.Host.Bindings;
+using Microsoft.Azure.WebJobs.Host.Listeners;
+using Microsoft.Azure.WebJobs.Host.Protocols;
+using Microsoft.Azure.WebJobs.Host.Triggers;
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.Azure.WebJobs.Extensions.Kafka.Trigger
+{
+    internal class KafkaTriggerBinding : ITriggerBinding
+    {
+        private readonly ParameterInfo _parameter;
+        private readonly ILogger _logger;
+        public KafkaTriggerBinding(ParameterInfo parameter, ILogger logger)
+        {
+            _parameter = parameter;
+            _logger = logger;
+        }
+
+        public Type TriggerValueType => throw new NotImplementedException();
+
+        public IReadOnlyDictionary<string, Type> BindingDataContract => throw new NotImplementedException();
+
+        public Task<ITriggerData> BindAsync(object value, ValueBindingContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Reliability", "CA2000:Dispose objects before losing scope")]
+        public Task<IListener> CreateListenerAsync(ListenerFactoryContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException("context", "Missing listener context");
+            }
+            
+            throw new NotImplementedException();
+        }
+
+        public ParameterDescriptor ToParameterDescriptor()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Microsoft.Azure.WebJobs.Extensions.Kafka/Trigger/KafkaTriggerListener.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs.Host.Executors;
+using Microsoft.Azure.WebJobs.Host.Listeners;
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.Azure.WebJobs.Extensions.Kafka.Trigger
+{
+    internal class KafkaTriggerListener : IListener
+    {
+        private const int ListenerNotRegistered = 0;
+        private const int ListenerRegistering = 1;
+        private const int ListenerRegistered = 2;
+
+        private readonly ITriggeredFunctionExecutor _executor;
+        private readonly ILogger _logger;
+
+        public KafkaTriggerListener(ITriggeredFunctionExecutor executor,
+            ILogger logger)
+        {
+            this._executor = executor;
+            this._logger = logger;
+        }
+        public void Cancel()
+        {
+            this.StopAsync(CancellationToken.None).Wait();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
